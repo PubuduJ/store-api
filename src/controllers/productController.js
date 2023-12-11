e0c537a0be65363f777def5927cefe9294a19fdc
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-11 11:08:10 +0530

    Implement sort functionality

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,19 +2,27 @@ const {StatusCodes} = require("http-status-codes");
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query;
-    const queryObject = {};
+    const {featured, company, name, sort} = req.query;
+    const filterObject = {};
     if (featured) {
-        queryObject.featured = (featured === "true") ? true : false;
+        filterObject.featured = (featured === "true") ? true : false;
     }
     if (company) {
-        queryObject.company = company;
+        filterObject.company = company;
     }
     if (name) {
         // search names based on regex, case insensitive.
-        queryObject.name = { $regex: name, $options: "i"};
+        filterObject.name = { $regex: name, $options: "i"};
     }
-    const products = await Product.find(queryObject);
+    let result = Product.find(filterObject);
+    if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        // default sort based on createdAt property.
+        result = result.sort("createdAt");
+    }
+    const products = await result;
     res.status(StatusCodes.OK).json({products, arrayLength: products.length});
 }
 
