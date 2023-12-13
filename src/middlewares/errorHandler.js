9b55157755b5d2dcf950ba7405de78762ee005a2
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-13 14:57:36 +0530

    Update errorHandler middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,32 @@
 const {StatusCodes} = require("http-status-codes");
 
 const errorHandler = async (err, req, res, next) => {
-  console.log(err)
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Something went wrong, please try again' })
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong, try again later"
+  }
+
+  // Handle cast errors
+  if (err.name === "CastError") {
+    customError.statusCode = StatusCodes.NOT_FOUND;
+    customError.message = `No item found with id: ${err.value}`;
+  }
+
+  // Handle validation errors
+  if (err.name === "ValidationError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = Object.values(err.errors).map((item) => {
+      return item.message;
+    }).join(", ");
+  }
+
+  // Handle duplicate errors
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+  }
+  return res.status(customError.statusCode).json({ message:customError.message });
 }
 
 module.exports = errorHandler;
